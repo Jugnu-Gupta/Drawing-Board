8bbe2997796275b1b4107ff9793ba28192d4bfb5
jugnugupta33 <jugnugupta33@gmail.com>
2024-05-26 12:53:55 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,3 +3,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
     base: "/vite-deploy/",
 });
+
+// // vite.config.js
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//     base: '',
+// })
